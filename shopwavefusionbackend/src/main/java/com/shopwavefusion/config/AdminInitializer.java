78adbfed2515c8bde8073a61774f4f39a676d376
Luis Aguilar <luis.aguilar.c@ucb.edu.bo>
2026-05-21 20:11:45 -0400

    fix

--- shopwavefusionbackend/src/main/java/com/shopwavefusion/config/AdminInitializer.java
+++ shopwavefusionbackend/src/main/java/com/shopwavefusion/config/AdminInitializer.java
@@ -8,24 +8,20 @@ import org.springframework.stereotype.Component;
 
 import com.shopwavefusion.modal.User;
 import com.shopwavefusion.repository.UserRepository;
-import com.shopwavefusion.service.CartService;
 
 @Component
 public class AdminInitializer implements CommandLineRunner {
 
     private final UserRepository userRepository;
-    private final CartService cartService;
     private final PasswordEncoder passwordEncoder;
 
-    public AdminInitializer(UserRepository userRepository, CartService cartService, PasswordEncoder passwordEncoder) {
+    public AdminInitializer(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
-        this.cartService = cartService;
         this.passwordEncoder = passwordEncoder;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // Check if admin user already exists
         if (userRepository.findByEmail("admin@example.com") == null) {
             User adminUser = new User();
             adminUser.setEmail("admin@example.com");
@@ -36,12 +32,7 @@ public class AdminInitializer implements CommandLineRunner {
             adminUser.setRole("ROLE_ADMIN");
             adminUser.setCreatedAt(LocalDateTime.now());
 
-            User savedAdminUser = userRepository.save(adminUser);
-
-          
-             cartService.createCart(savedAdminUser);
-           
+            userRepository.save(adminUser);
         }
     }
 }
-
